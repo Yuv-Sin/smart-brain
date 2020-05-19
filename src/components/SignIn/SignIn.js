9ec25d3721055917e8f6d8bb648c3d8694ff43f9
Yuvraj Singh Rana <yuvi1690@gmail.com>
2020-05-19 21:50:18 +0530

    some beauty modifications

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -41,6 +41,7 @@ class SignIn extends Component {
 	render() {
 		const { onRouteChange } = this.props;
 		return(
+			<h1 className="f1 tc">Smart Brain</h1>
 		<article class="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
 				<main className="pa4 black-80">
 				  <div className="measure">
