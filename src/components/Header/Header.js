43308d8d6630efd6b8b36e0a9b2e61389a3030e4
Yuvraj Singh Rana <yuvi1690@gmail.com>
2020-05-20 13:51:40 +0530

    ok

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.png'
 class Header extends Component{
-	render(
+	render(){
+		return(
 		<div>
 		<img  className="br2 shadow-2" style={{height: 200, width: 200}} alt='Logo' src={logo} />
 		</div>
 		);
+		}
 }
 export default Header;
