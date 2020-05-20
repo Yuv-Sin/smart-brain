da893ed8d950907cf4f4bc2fdb948c0700e095de
Yuvraj Singh Rana <yuvi1690@gmail.com>
2020-05-20 14:14:05 +0530

    ok

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,10 +4,10 @@ import brain from './brain.png'
 import './Logo.css';
 const Logo = () =>{
 	return(
-		<div className='ma4 mt0 tc'>
+		<div className='ma4 mt0'>
 			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 200, width: 200 }} >
  			<div className="Tilt-inner pa3"><img style={{paddingTop: '30px'}} alt='brain' src={brain}/></div>
-			</Tilt>
+</Tilt>
 		</div>
 		);
 }
